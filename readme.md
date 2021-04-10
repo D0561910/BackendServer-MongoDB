aab29ad2177787909679fa22144c7240e962e6ee
d0561910 <d0561910@mail.fcu.edu.tw>
2021-04-10 13:37:38 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## Basic Backend Server
+
+### Run application
+
+```
+npm install && npm start
+```
+
+This is a todolist API Server\
+Database: Mongo DB\
+Server: Express\
--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+
+const MONGO_URI =
+  "";
 
 const connectMongoDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://d0561910:93112808Yiew@cluster0.eesp5.mongodb.net/todolistDB?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    );
+    const conn = await mongoose.connect(MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error : ${error.message}`.red.underline.bold);
